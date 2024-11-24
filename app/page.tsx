6761fac26b574840b34b51aacb55fcf8a098106c
Chris Gregory <gregorybchris@gmail.com>
2024-11-24 12:19:16 -0800

    New icon

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,11 @@ export default function SplashPage() {
     <div className="flex h-screen w-screen items-center justify-center">
       <div className="flex flex-col items-center justify-center gap-10">
         <span>Tufts University Cross Country</span>
-        <LinkButton text="Run Preservation Project" href="/rpp" />
+        <LinkButton
+          text="Run Preservation Project"
+          href="/rpp"
+          iconName="rpp"
+        />
       </div>
     </div>
   );
