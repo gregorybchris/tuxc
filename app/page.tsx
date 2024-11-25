f8dca03ec1cfcb7c1742d7f7879becd1bebfffdb
Chris Gregory <gregorybchris@gmail.com>
2024-11-24 22:15:23 -0800

    Change spacing

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import { LinkButton } from "./widgets/link-button";
 
 export default function SplashPage() {
   return (
-    <div className="h-full w-full px-5 py-10 md:px-20 md:py-20">
-      <div className="flex flex-col items-center justify-center gap-10">
-        <span>Tufts University Cross Country</span>
+    <div className="h-full w-full px-5 py-20 md:px-20 md:py-28">
+      <div className="flex flex-col items-center justify-center gap-6">
+        <span className="text-xl">Tufts University Cross Country</span>
         <LinkButton
           text="Run Preservation Project"
           href="/rpp"
