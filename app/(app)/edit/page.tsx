b6d6b8349cfc3b25c330025fe2baad45c8a3502f
Chris Gregory <gregorybchris@gmail.com>
2024-11-26 04:02:00 -0800

    More on edit page

--- app/(app)/edit/page.tsx
+++ app/(app)/edit/page.tsx
@@ -5,11 +5,16 @@ import { useEffect, useRef, useState } from "react";
 import { LoadingRunViews } from "@/app/components/loading-run-views";
 import { Client } from "@/app/lib/clients/client";
 import { Run } from "@/app/lib/models/run";
+import { Button } from "@/app/widgets/button";
+import { Textbox } from "@/app/widgets/textbox";
 
 export default function EditPage() {
   const [loading, setLoading] = useState(true);
   const [runs, setRuns] = useState<Run[]>([]);
   const client = useRef(new Client());
+  const [descriptionText, setDescriptionText] = useState("");
+  const [loreText, setLoreText] = useState("");
+  const [somethingElseText, setSomethingElseText] = useState("");
 
   useEffect(() => {
     fetchRuns();
@@ -25,6 +30,10 @@ export default function EditPage() {
     });
   }
 
+  function onSubmit() {
+    // Do something with the text values
+  }
+
   return (
     <div className="h-full w-full px-5 py-10 md:px-20 md:py-20">
       <div className="flex w-full flex-col items-center gap-5 md:items-start">
@@ -34,9 +43,26 @@ export default function EditPage() {
         {loading && <LoadingRunViews numLoading={10} />}
 
         {!loading && (
-          <div className="flex flex-col gap-6">
-            <div>How to make an edit</div>
-            <div>You can't make edits yet. Check back later!</div>
+          <div className="flex w-full flex-col items-center gap-6">
+            <div>To edit a run, fill out some or all of the fields below.</div>
+            <div className="flex w-full flex-col gap-3 md:w-[400px]">
+              <Textbox
+                value={descriptionText}
+                onChange={setDescriptionText}
+                placeholder="Description"
+              />
+              <Textbox
+                value={loreText}
+                onChange={setLoreText}
+                placeholder="Lore"
+              />
+              <Textbox
+                value={somethingElseText}
+                onChange={setSomethingElseText}
+                placeholder="Something else?"
+              />
+              <Button onClick={onSubmit} text="Submit" />
+            </div>
           </div>
         )}
       </div>
