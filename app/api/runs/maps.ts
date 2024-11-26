9a41ba56f36a1821a28c41bf3c8baf5518390600
Chris Gregory <gregorybchris@gmail.com>
2024-11-26 02:06:03 -0800

    Add new runs

--- app/api/runs/maps.ts
+++ app/api/runs/maps.ts
@@ -1,63 +1,317 @@
 import { RunMap } from "@/app/lib/models/runMap";
+
 import MAP_09_RED_LINE from "./maps/09-red-line.json";
-import MAP_09_SPY_LINE from "./maps/09-spy-line.json";
+// import MAP_1500_METER_POND_SHORT from "./maps/1500-meter-pond-short.json";
+import MAP_1500_METER_POND from "./maps/1500-meter-pond.json";
+// import MAP_2_CENTRAL from "./maps/2-central.json";
+import MAP_6_MILE_BIKE_PATH from "./maps/6-mile-bike-path.json";
+import MAP_96_STRAIGHT_STREET from "./maps/96-straight-street.json";
+import MAP_ALEWIFE_GREENWAY from "./maps/alewife-greenway.json";
+import MAP_AQUARIUM from "./maps/aquarium.json";
+import MAP_ARBORETUM_X_JP from "./maps/arboretum-x-jp.json";
+import MAP_ARLINGTON_TO_ALEWIFE from "./maps/arlington-to-alewife.json";
+import MAP_AROUND_THE_FELLS from "./maps/around-the-fells.json";
+import MAP_ASS_CANYON from "./maps/ass-canyon.json";
 import MAP_BATTLE_ROAD from "./maps/battle-road.json";
-import MAP_BELMONT_LAKES from "./maps/belmont-lakes.json";
+import MAP_BBB from "./maps/bbb.json";
+// import MAP_BC_RES from "./maps/bc-res.json";
+import MAP_BELMONT_HILL from "./maps/belmont-hill.json";
 import MAP_BELMONT from "./maps/belmont.json";
+import MAP_BERNIES_GREAT_MEADOW from "./maps/bernies-great-meadow.json";
+// import MAP_BFL from "./maps/bfl.json";
+// import MAP_BIKE_LAKES from "./maps/bike-lakes.json";
+import MAP_BOAT_HOUSE from "./maps/boat-house.json";
+// import MAP_BOSTON_MASSACRE from "./maps/boston-massacre.json";
+// import MAP_BOSTON_TOUR from "./maps/boston-tour.json";
+import MAP_BRATTLE_STREET from "./maps/brattle-street.json";
+import MAP_BROADWASH from "./maps/broadwash.json";
+import MAP_BROOKS_ESTATE from "./maps/brooks-estate.json";
 import MAP_BU_BRIDGE_FAKE_10 from "./maps/bu-bridge-fake-10.json";
+import MAP_BUNKER_HILL from "./maps/bunker-hill.json";
 import MAP_C_LOOP from "./maps/c-loop.json";
+import MAP_CASTLE_BLACK from "./maps/castle-black.json";
 import MAP_CEMETERY from "./maps/cemetery.json";
+// import MAP_CHARLES_NINE_MILER from "./maps/charles-nine-miler.json";
+// import MAP_CHARLES_ST from "./maps/charles-st.json";
 import MAP_CHIPS from "./maps/chips.json";
+import MAP_CHRIS_DADS_HOUSE from "./maps/chris-dads-house.json";
+// import MAP_DEA_POND from "./maps/dea-pond.json";
+import MAP_DO_IT_LIVE from "./maps/do-it-live.json";
+// import MAP_DONUT_RUN_DELUXE from "./maps/donut-run-deluxe.json";
+// import MAP_DOUBLE_MID_RES from "./maps/double-mid-res.json";
+import MAP_ENCORE from "./maps/encore.json";
+import MAP_ENFORCER from "./maps/enforcer.json";
+// import MAP_EX_BFL from "./maps/ex-bfl.json";
 import MAP_EXTENDED_CEMETERY from "./maps/extended-cemetery.json";
 import MAP_EXTENDED_CHIPS from "./maps/extended-chips.json";
-import MAP_FRESH_POND_2_LAPS from "./maps/fresh-pond-2-laps.json";
+import MAP_EXTENDED_HARVARD from "./maps/extended-harvard.json";
+import MAP_EXTENDEDLY_SHORT_GLAM from "./maps/extendedly-short-glam.json";
+import MAP_EXTENDEDLY_SHORTER_GLAM from "./maps/extendedly-shorter-glam.json";
+// import MAP_EXTRA_SHORT_FREEDOM_TRAIL from "./maps/extra-short-freedom-trail.json";
+import MAP_FAR_TOWER_TO_FIREHOUSE from "./maps/far-tower-to-firehouse.json";
+import MAP_FAR_TOWER from "./maps/far-tower.json";
+// import MAP_FELLS_N_CHIPS from "./maps/fells-n-chips.json";
+import MAP_FELLSWAY from "./maps/fellsway.json";
+// import MAP_FEMEPOND_CHARLES_BFL from "./maps/femepond-charles-bfl.json";
+import MAP_FEMETERY from "./maps/femetery.json";
+import MAP_FEMMED from "./maps/femmed.json";
+import MAP_FENWAY from "./maps/fenway.json";
+import MAP_FIREHOUSE from "./maps/firehouse.json";
+// import MAP_FREEDOM_TRAIL_EXTENDED from "./maps/freedom-trail-extended.json";
+// import MAP_FREEDOM_TRAIL_SHORT from "./maps/freedom-trail-short.json";
+import MAP_FREEDOM_TRAIL from "./maps/freedom-trail.json";
 import MAP_FRESH_POND from "./maps/fresh-pond.json";
+import MAP_FRIES from "./maps/fries.json";
 import MAP_FULL_GLAM from "./maps/full-glam.json";
+import MAP_FUMRD from "./maps/fumrd.json";
+import MAP_GC from "./maps/gc.json";
+import MAP_GEORGE_STREET from "./maps/george-street.json";
+import MAP_GHETTO_RUN_0_9_REDLINE_SUCKS from "./maps/ghetto-run-0-9-redline-sucks.json";
+import MAP_GLAMOURVILLE from "./maps/glamourville.json";
+import MAP_GOLDILOCKS from "./maps/goldilocks.json";
 import MAP_GOLDMAN_SACHS from "./maps/goldman-sachs.json";
+import MAP_GOOCH_PLAYGROUND from "./maps/gooch-playground.json";
+import MAP_GREENWAY from "./maps/greenway.json";
+import MAP_HABITAT from "./maps/habitat.json";
 import MAP_HARVARD from "./maps/harvard.json";
 import MAP_HB from "./maps/hb.json";
+import MAP_HOOVER_SUCKS from "./maps/hoover-sucks.json";
+import MAP_HORN_POND from "./maps/horn-pond.json";
+import MAP_JP from "./maps/jp.json";
+import MAP_K_HARVARD from "./maps/k-harvard.json";
 import MAP_LAKES from "./maps/lakes.json";
+import MAP_LOGAN_FERRY from "./maps/logan-ferry.json";
+import MAP_LONG_LAKES from "./maps/long-lakes.json";
+// import MAP_LONG_RES_TO_LONG_LAKES from "./maps/long-res-to-long-lakes.json";
+import MAP_LONG_RES_TO_WINCHESTER_HOSPITAL from "./maps/long-res-to-winchester-hospital.json";
 import MAP_LONG_RES from "./maps/long-res.json";
+import MAP_LONGFELLOW from "./maps/longfellow.json";
 import MAP_MAIN_STREET from "./maps/main-street.json";
+import MAP_MALDEN from "./maps/malden.json";
+// import MAP_MARATHON_MONDAY from "./maps/marathon-monday.json";
+import MAP_MARVELS_D from "./maps/marvels-d.json";
 import MAP_MED_SQUARE from "./maps/med-square.json";
+import MAP_MENOTOMY_TO_BELMONT from "./maps/menotomy-to-belmont.json";
+// import MAP_MENOTOMY from "./maps/menotomy.json";
+import MAP_MET_STATE from "./maps/met-state.json";
+// import MAP_MID_D_MADNESS from "./maps/mid-d-madness.json";
 import MAP_MID_RES from "./maps/mid-res.json";
+import MAP_MOMS_RUN from "./maps/moms-run.json";
+// import MAP_MUSEUM_OF_SCIENCE from "./maps/museum-of-science.json";
 import MAP_MYSTIC_STOPLIGHT_COOLDOWN from "./maps/mystic-stoplight-cooldown.json";
 import MAP_NEAR_TOWER from "./maps/near-tower.json";
+import MAP_NEW_BUNKER_HILL from "./maps/new-bunker-hill.json";
+import MAP_NO_FELLS from "./maps/no-fells.json";
+import MAP_NO_HILL from "./maps/no-hill.json";
+import MAP_NO_RES from "./maps/no-res.json";
+// import MAP_NO_WHIP from "./maps/no-whip.json";
+import MAP_NORTH_STREET from "./maps/north-street.json";
+import MAP_OG_NO_FELLS from "./maps/og-no-fells.json";
+import MAP_OG_NO_HILL from "./maps/og-no-hill.json";
+import MAP_OGSB from "./maps/ogsb.json";
+import MAP_ONE_BRIDGE from "./maps/one-bridge.json";
+import MAP_PISGAH from "./maps/pisgah.json";
+import MAP_PORTER from "./maps/porter.json";
+import MAP_POWDER_TEELE from "./maps/powder-teele.json";
+import MAP_PRESIDENTS_RUN from "./maps/presidents-run.json";
+// import MAP_PURPLE_PEOPLE_EATER from "./maps/purple-people-eater.json";
+import MAP_RAILROAD_UNDERPASS from "./maps/railroad-underpass.json";
+import MAP_RECTANGLE from "./maps/rectangle.json";
+import MAP_RUBI_FACTORY from "./maps/rubi-factory.json";
+import MAP_S_BIASED from "./maps/s-biased.json";
+// import MAP_SANDY_BEACH from "./maps/sandy-beach.json";
+import MAP_SAUDI_PRINCE from "./maps/saudi-prince.json";
+import MAP_SHAKER from "./maps/shaker.json";
+// import MAP_SHORT_FENWAY from "./maps/short-fenway.json";
 import MAP_SHORT_GLAM from "./maps/short-glam.json";
+// import MAP_SHORT_RES_TO_BU_BRIDGE from "./maps/short-res-to-bu-bridge.json";
 import MAP_SHORT_RES from "./maps/short-res.json";
+import MAP_SHORT_SB from "./maps/short-sb.json";
+import MAP_SKELETON_OVERLOOK from "./maps/skeleton-overlook.json";
+import MAP_SMOOTS from "./maps/smoots.json";
+import MAP_SON_OF_A_BEACH from "./maps/son-of-a-beach.json";
+import MAP_SOUTH_BORDER_ROAD from "./maps/south-border-road.json";
+import MAP_SOUTH_OF_THE_BORDER from "./maps/south-of-the-border.json";
 import MAP_SPY_POND from "./maps/spy-pond.json";
+import MAP_SQUARES from "./maps/squares.json";
 import MAP_SWIMMING_HOLE from "./maps/swimming-hole.json";
+import MAP_SY_SNOOTLES from "./maps/sy-snootles.json";
+import MAP_TAYLORS_STUPID_RUN from "./maps/taylors-stupid-run.json";
+import MAP_THE_HIGHLANDS from "./maps/the-highlands.json";
+import MAP_TOUR_DE_CAMBRVILLE_BRICKS from "./maps/tour-de-cambrville-bricks.json";
+import MAP_TOUR_DE_MALDEN from "./maps/tour-de-malden.json";
+import MAP_TOWNIE_PORN from "./maps/townie-porn.json";
+// import MAP_TRIPLE_PEAKS from "./maps/triple-peaks.json";
+import MAP_TRUNKS from "./maps/trunks.json";
+// import MAP_TWIN_PEAKS from "./maps/twin-peaks.json";
 import MAP_TWO_BRIDGES from "./maps/two-bridges.json";
+import MAP_VIKING_TOWER from "./maps/viking-tower.json";
+import MAP_WEST_CAMBRIDGE from "./maps/west-cambridge.json";
+import MAP_WEST_LAKES from "./maps/west-lakes.json";
+import MAP_WEST_MED from "./maps/west-med.json";
+import MAP_WILSON_FARM from "./maps/wilson-farm.json";
+// import MAP_WINCHESTER_HOSPITAL from "./maps/winchester-hospital.json";
+import MAP_WOODBINE_WIGGLE from "./maps/woodbine-wiggle.json";
+import MAP_X_C_LOOP from "./maps/x-c-loop.json";
+import MAP_X_NO_HILL from "./maps/x-no-hill.json";
+// import MAP_YERXA from "./maps/yerxa.json";
+import MAP_ZERO_FELLS from "./maps/zero-fells.json";
 
 export const MAPS = new Map<string, RunMap>([
   ["09-red-line", MAP_09_RED_LINE],
-  ["09-red-line", MAP_09_RED_LINE],
-  ["09-spy-line", MAP_09_SPY_LINE],
+  // ["1500-meter-pond-short", MAP_1500_METER_POND_SHORT],
+  ["1500-meter-pond", MAP_1500_METER_POND],
+  // ["2-central", MAP_2_CENTRAL],
+  ["6-mile-bike-path", MAP_6_MILE_BIKE_PATH],
+  ["96-straight-street", MAP_96_STRAIGHT_STREET],
+  ["alewife-greenway", MAP_ALEWIFE_GREENWAY],
+  ["aquarium", MAP_AQUARIUM],
+  ["arboretum-x-jp", MAP_ARBORETUM_X_JP],
+  ["arlington-to-alewife", MAP_ARLINGTON_TO_ALEWIFE],
+  ["around-the-fells", MAP_AROUND_THE_FELLS],
+  ["ass-canyon", MAP_ASS_CANYON],
   ["battle-road", MAP_BATTLE_ROAD],
-  ["belmont-lakes", MAP_BELMONT_LAKES],
+  ["bbb", MAP_BBB],
+  // ["bc-res", MAP_BC_RES],
+  ["belmont-hill", MAP_BELMONT_HILL],
   ["belmont", MAP_BELMONT],
+  ["bernies-great-meadow", MAP_BERNIES_GREAT_MEADOW],
+  // ["bfl", MAP_BFL],
+  // ["bike-lakes", MAP_BIKE_LAKES],
+  ["boat-house", MAP_BOAT_HOUSE],
+  // ["boston-massacre", MAP_BOSTON_MASSACRE],
+  // ["boston-tour", MAP_BOSTON_TOUR],
+  ["brattle-street", MAP_BRATTLE_STREET],
+  ["broadwash", MAP_BROADWASH],
+  ["brooks-estate", MAP_BROOKS_ESTATE],
   ["bu-bridge-fake-10", MAP_BU_BRIDGE_FAKE_10],
+  ["bunker-hill", MAP_BUNKER_HILL],
   ["c-loop", MAP_C_LOOP],
+  ["castle-black", MAP_CASTLE_BLACK],
   ["cemetery", MAP_CEMETERY],
+  // ["charles-nine-miler", MAP_CHARLES_NINE_MILER],
+  // ["charles-st", MAP_CHARLES_ST],
   ["chips", MAP_CHIPS],
+  ["chris-dads-house", MAP_CHRIS_DADS_HOUSE],
+  // ["dea-pond", MAP_DEA_POND],
+  ["do-it-live", MAP_DO_IT_LIVE],
+  // ["donut-run-deluxe", MAP_DONUT_RUN_DELUXE],
+  // ["double-mid-res", MAP_DOUBLE_MID_RES],
+  ["encore", MAP_ENCORE],
+  ["enforcer", MAP_ENFORCER],
+  // ["ex-bfl", MAP_EX_BFL],
   ["extended-cemetery", MAP_EXTENDED_CEMETERY],
   ["extended-chips", MAP_EXTENDED_CHIPS],
-  ["fresh-pond-2-laps", MAP_FRESH_POND_2_LAPS],
+  ["extended-harvard", MAP_EXTENDED_HARVARD],
+  ["extendedly-short-glam", MAP_EXTENDEDLY_SHORT_GLAM],
+  ["extendedly-shorter-glam", MAP_EXTENDEDLY_SHORTER_GLAM],
+  // ["extra-short-freedom-trail", MAP_EXTRA_SHORT_FREEDOM_TRAIL],
+  ["far-tower-to-firehouse", MAP_FAR_TOWER_TO_FIREHOUSE],
+  ["far-tower", MAP_FAR_TOWER],
+  // ["fells-n-chips", MAP_FELLS_N_CHIPS],
+  ["fellsway", MAP_FELLSWAY],
+  // ["femepond-charles-bfl", MAP_FEMEPOND_CHARLES_BFL],
+  ["femetery", MAP_FEMETERY],
+  ["femmed", MAP_FEMMED],
+  ["fenway", MAP_FENWAY],
+  ["firehouse", MAP_FIREHOUSE],
+  // ["freedom-trail-extended", MAP_FREEDOM_TRAIL_EXTENDED],
+  // ["freedom-trail-short", MAP_FREEDOM_TRAIL_SHORT],
+  ["freedom-trail", MAP_FREEDOM_TRAIL],
   ["fresh-pond", MAP_FRESH_POND],
+  ["fries", MAP_FRIES],
   ["full-glam", MAP_FULL_GLAM],
+  ["fumrd", MAP_FUMRD],
+  ["gc", MAP_GC],
+  ["george-street", MAP_GEORGE_STREET],
+  ["ghetto-run-0-9-redline-sucks", MAP_GHETTO_RUN_0_9_REDLINE_SUCKS],
+  ["glamourville", MAP_GLAMOURVILLE],
+  ["goldilocks", MAP_GOLDILOCKS],
   ["goldman-sachs", MAP_GOLDMAN_SACHS],
+  ["gooch-playground", MAP_GOOCH_PLAYGROUND],
+  ["greenway", MAP_GREENWAY],
+  ["habitat", MAP_HABITAT],
   ["harvard", MAP_HARVARD],
   ["hb", MAP_HB],
+  ["hoover-sucks", MAP_HOOVER_SUCKS],
+  ["horn-pond", MAP_HORN_POND],
+  ["jp", MAP_JP],
+  ["k-harvard", MAP_K_HARVARD],
   ["lakes", MAP_LAKES],
+  ["logan-ferry", MAP_LOGAN_FERRY],
+  ["long-lakes", MAP_LONG_LAKES],
+  // ["long-res-to-long-lakes", MAP_LONG_RES_TO_LONG_LAKES],
+  ["long-res-to-winchester-hospital", MAP_LONG_RES_TO_WINCHESTER_HOSPITAL],
   ["long-res", MAP_LONG_RES],
+  ["longfellow", MAP_LONGFELLOW],
   ["main-street", MAP_MAIN_STREET],
+  ["malden", MAP_MALDEN],
+  // ["marathon-monday", MAP_MARATHON_MONDAY],
+  ["marvels-d", MAP_MARVELS_D],
   ["med-square", MAP_MED_SQUARE],
+  ["menotomy-to-belmont", MAP_MENOTOMY_TO_BELMONT],
+  // ["menotomy", MAP_MENOTOMY],
+  ["met-state", MAP_MET_STATE],
+  // ["mid-d-madness", MAP_MID_D_MADNESS],
   ["mid-res", MAP_MID_RES],
+  ["moms-run", MAP_MOMS_RUN],
+  // ["museum-of-science", MAP_MUSEUM_OF_SCIENCE],
   ["mystic-stoplight-cooldown", MAP_MYSTIC_STOPLIGHT_COOLDOWN],
   ["near-tower", MAP_NEAR_TOWER],
+  ["new-bunker-hill", MAP_NEW_BUNKER_HILL],
+  ["no-fells", MAP_NO_FELLS],
+  ["no-hill", MAP_NO_HILL],
+  ["no-res", MAP_NO_RES],
+  // ["no-whip", MAP_NO_WHIP],
+  ["north-street", MAP_NORTH_STREET],
+  ["og-no-fells", MAP_OG_NO_FELLS],
+  ["og-no-hill", MAP_OG_NO_HILL],
+  ["ogsb", MAP_OGSB],
+  ["one-bridge", MAP_ONE_BRIDGE],
+  ["pisgah", MAP_PISGAH],
+  ["porter", MAP_PORTER],
+  ["powder-teele", MAP_POWDER_TEELE],
+  ["presidents-run", MAP_PRESIDENTS_RUN],
+  // ["purple-people-eater", MAP_PURPLE_PEOPLE_EATER],
+  ["railroad-underpass", MAP_RAILROAD_UNDERPASS],
+  ["rectangle", MAP_RECTANGLE],
+  ["rubi-factory", MAP_RUBI_FACTORY],
+  ["s-biased", MAP_S_BIASED],
+  // ["sandy-beach", MAP_SANDY_BEACH],
+  ["saudi-prince", MAP_SAUDI_PRINCE],
+  ["shaker", MAP_SHAKER],
+  // ["short-fenway", MAP_SHORT_FENWAY],
   ["short-glam", MAP_SHORT_GLAM],
+  // ["short-res-to-bu-bridge", MAP_SHORT_RES_TO_BU_BRIDGE],
   ["short-res", MAP_SHORT_RES],
+  ["short-sb", MAP_SHORT_SB],
+  ["skeleton-overlook", MAP_SKELETON_OVERLOOK],
+  ["smoots", MAP_SMOOTS],
+  ["son-of-a-beach", MAP_SON_OF_A_BEACH],
+  ["south-border-road", MAP_SOUTH_BORDER_ROAD],
+  ["south-of-the-border", MAP_SOUTH_OF_THE_BORDER],
   ["spy-pond", MAP_SPY_POND],
+  ["squares", MAP_SQUARES],
   ["swimming-hole", MAP_SWIMMING_HOLE],
+  ["sy-snootles", MAP_SY_SNOOTLES],
+  ["taylors-stupid-run", MAP_TAYLORS_STUPID_RUN],
+  ["the-highlands", MAP_THE_HIGHLANDS],
+  ["tour-de-cambrville-bricks", MAP_TOUR_DE_CAMBRVILLE_BRICKS],
+  ["tour-de-malden", MAP_TOUR_DE_MALDEN],
+  ["townie-porn", MAP_TOWNIE_PORN],
+  // ["triple-peaks", MAP_TRIPLE_PEAKS],
+  ["trunks", MAP_TRUNKS],
+  // ["twin-peaks", MAP_TWIN_PEAKS],
   ["two-bridges", MAP_TWO_BRIDGES],
+  ["viking-tower", MAP_VIKING_TOWER],
+  ["west-cambridge", MAP_WEST_CAMBRIDGE],
+  ["west-lakes", MAP_WEST_LAKES],
+  ["west-med", MAP_WEST_MED],
+  ["wilson-farm", MAP_WILSON_FARM],
+  // ["winchester-hospital", MAP_WINCHESTER_HOSPITAL],
+  ["woodbine-wiggle", MAP_WOODBINE_WIGGLE],
+  ["x-c-loop", MAP_X_C_LOOP],
+  ["x-no-hill", MAP_X_NO_HILL],
+  // ["yerxa", MAP_YERXA],
+  ["zero-fells", MAP_ZERO_FELLS],
 ]);
