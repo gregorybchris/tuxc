2afbf1bf08a3c8e042b596b5b84066b736aac2e3
Chris Gregory <gregorybchris@gmail.com>
2024-11-26 03:53:55 -0800

    Revert thumbnail

---
+++ app/components/run-thumbnail.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { Run } from "@/app/lib/models/run";
+import { useEffect, useRef, useState } from "react";
+import { Client } from "../lib/clients/client";
+import { RunMap } from "../lib/models/runMap";
+import { generateThumbnail } from "../lib/thumbnail/thumbnail-generator";
+
+interface RunViewProps {
+  run: Run;
+  className?: string;
+}
+
+export function RunView({ run, className }: RunViewProps) {
+  const [runMap, setRunMap] = useState<RunMap>();
+  const thumbnailRef = useRef<SVGSVGElement | null>(null);
+  const client = useRef(new Client());
+
+  useEffect(() => {
+    const svg = thumbnailRef.current;
+    if (!svg) return;
+
+    if (!runMap) return;
+
+    while (svg.firstChild) {
+      svg.removeChild(svg.firstChild);
+    }
+
+    generateThumbnail(runMap, svg);
+  }, [runMap]);
+
+  useEffect(() => {
+    fetchRun();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  function fetchRun() {
+    client.current.getRunMap(run.id).then((runMap) => {
+      setRunMap(runMap);
+    });
+  }
+
+  return (
+    <svg
+      ref={thumbnailRef}
+      className="h-20 w-full border-4 border-transparent transition-all group-hover:border-white/40 md:h-24"
+    />
+  );
+}
--- app/components/run-view.tsx
+++ app/components/run-view.tsx
@@ -3,10 +3,6 @@
 import { Run } from "@/app/lib/models/run";
 import { cn } from "@/app/lib/utilities/style-utils";
 import Link from "next/link";
-import { useEffect, useRef, useState } from "react";
-import { Client } from "../lib/clients/client";
-import { RunMap } from "../lib/models/runMap";
-import { generateThumbnail } from "../lib/thumbnail/thumbnail-generator";
 
 interface RunViewProps {
   run: Run;
@@ -14,42 +10,11 @@ interface RunViewProps {
 }
 
 export function RunView({ run, className }: RunViewProps) {
-  const [runMap, setRunMap] = useState<RunMap>();
-  const thumbnailRef = useRef<SVGSVGElement | null>(null);
-  const client = useRef(new Client());
-
-  useEffect(() => {
-    const svg = thumbnailRef.current;
-    if (!svg) return;
-
-    if (!runMap) return;
-
-    while (svg.firstChild) {
-      svg.removeChild(svg.firstChild);
-    }
-
-    generateThumbnail(runMap, svg);
-  }, [runMap]);
-
-  useEffect(() => {
-    fetchRun();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  function fetchRun() {
-    client.current.getRunMap(run.id).then((runMap) => {
-      setRunMap(runMap);
-    });
-  }
-
   return (
     <div className={cn("group w-40 transition-all md:w-36", className)}>
       <Link href={`/runs/${run.id}`}>
         <div className="flex flex-col items-start gap-2">
-          <svg
-            ref={thumbnailRef}
-            className="h-20 w-full border-4 border-transparent transition-all group-hover:border-white/40 md:h-24"
-          />
+          <div className="flex h-20 w-full flex-row items-center justify-center rounded-md border-4 border-transparent bg-tufts-brown transition-all group-hover:border-white/40 md:h-24"></div>
           <div className="text-near-white text-sm transition-all group-hover:text-black/70">
             {run.name}
           </div>
