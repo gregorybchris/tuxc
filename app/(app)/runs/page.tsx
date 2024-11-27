9e304fbc41f8d568beef48f9f0752441b14b1b82
Chris Gregory <gregorybchris@gmail.com>
2024-11-26 17:24:59 -0800

    Add slider to filter runs by distance

--- app/(app)/runs/page.tsx
+++ app/(app)/runs/page.tsx
@@ -15,6 +15,10 @@ import {
 } from "@/app/lib/utilities/sort-utils";
 import { LinkButton } from "@/app/widgets/link-button";
 import { Textbox } from "@/app/widgets/textbox";
+import * as Slider from "@radix-ui/react-slider";
+
+const MIN_DISTANCE = 0;
+const MAX_DISTANCE = 100;
 
 export default function RunsPage() {
   const [loading, setLoading] = useState(true);
@@ -22,6 +26,10 @@ export default function RunsPage() {
   const [runs, setRuns] = useState<Run[]>([]);
   const [sortValue, setSortValue] = useState<string>("Alphabetical");
   const client = useRef(new Client());
+  const [distanceRange, setDistanceRange] = useState([
+    MIN_DISTANCE,
+    MAX_DISTANCE,
+  ]);
 
   useEffect(() => {
     fetchRuns();
@@ -33,6 +41,7 @@ export default function RunsPage() {
     setRuns([]);
     client.current.getRuns().then((runs) => {
       setRuns(runs);
+      setDistanceRange(getDistanceRange(runs));
       setLoading(false);
     });
   }
@@ -46,6 +55,23 @@ export default function RunsPage() {
     selectedRuns = getRunsByDistance(selectedRuns);
   }
 
+  selectedRuns = selectedRuns.filter(
+    (run) =>
+      run.distance >= distanceRange[0] && run.distance <= distanceRange[1],
+  );
+
+  function getDistanceRange(runs: Run[]): [number, number] {
+    const minDistance =
+      runs.reduce((min, run) => Math.min(min, run.distance), MAX_DISTANCE) ||
+      MIN_DISTANCE;
+    const maxDistance =
+      runs.reduce((max, run) => Math.max(max, run.distance), MIN_DISTANCE) ||
+      MAX_DISTANCE;
+    return [minDistance, maxDistance];
+  }
+
+  const [minDistance, maxDistance] = getDistanceRange(runs);
+
   return (
     <div className="h-full w-full px-5 py-10 md:px-20 md:py-20">
       <div className="flex w-full flex-col items-center gap-5 md:items-start">
@@ -54,10 +80,41 @@ export default function RunsPage() {
             TUXC Run Preservation Project
           </div>
         </div>
-        <div className="flex w-full flex-col items-center gap-2 px-10 md:items-start md:px-0">
-          <LinkButton text="About the project" href="/rpp" iconName="info" />
-          <LinkButton text="Submit a run" href="/edit" iconName="pin-plus" />
-          <div className="flex w-full flex-col items-center gap-3 md:flex-row">
+        <div className="flex w-full flex-col items-center gap-6 px-10 md:items-start md:px-0">
+          <div className="flex flex-row items-center gap-5">
+            <LinkButton text="About the project" href="/rpp" iconName="info" />
+            <LinkButton text="Submit a run" href="/edit" iconName="pin-plus" />
+          </div>
+          <div className="flex w-full flex-col items-center gap-5 md:flex-row">
+            <div className="flex flex-row gap-3">
+              <div className="text-sm">Distance</div>
+              <Slider.Root
+                className="relative flex h-5 w-[200px] touch-none select-none items-center"
+                min={minDistance}
+                max={maxDistance}
+                step={1}
+                minStepsBetweenThumbs={1}
+                value={distanceRange}
+                onValueChange={setDistanceRange}
+              >
+                <Slider.Track className="relative h-[3px] grow rounded-full bg-black/20">
+                  <Slider.Range className="absolute h-full rounded-full bg-black/30" />
+                </Slider.Track>
+                <Slider.Thumb className="bg-light-blue flex size-5 cursor-pointer flex-col items-center justify-center rounded-[10px] text-[10px] text-white hover:bg-tufts-blue focus:outline-none">
+                  {distanceRange[0].toFixed(0)}
+                </Slider.Thumb>
+                <Slider.Thumb className="bg-light-blue flex size-5 cursor-pointer flex-col items-center justify-center rounded-[10px] text-[10px] text-white hover:bg-tufts-blue focus:outline-none">
+                  {distanceRange[1].toFixed(0)}
+                </Slider.Thumb>
+              </Slider.Root>
+            </div>
+
+            <Dropdown
+              value={sortValue}
+              setValue={setSortValue}
+              choices={["Alphabetical", "Distance", "Recently added"]}
+            />
+
             <Textbox
               value={searchText}
               onChange={setSearchText}
@@ -67,12 +124,6 @@ export default function RunsPage() {
               required
               icon="search"
               className="w-full md:w-[300px]"
-            />
-
-            <Dropdown
-              value={sortValue}
-              setValue={setSortValue}
-              choices={["Alphabetical", "Distance", "Recently added"]}
             />
           </div>
         </div>
